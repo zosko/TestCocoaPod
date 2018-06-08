7b5f77e2c22b7ac7d7873ae6043dfd8191eee27c
Bosko Petreski <bosko@Boskos-MacBook-Pro.local>
2018-06-08 15:28:06 +0200

    init omit

---
+++ DemoPod/Example/DemoPod/DEMOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEMOAppDelegate.h
+//  DemoPod
+//
+//  Created by zosko on 06/08/2018.
+//  Copyright (c) 2018 zosko. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DEMOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoPod/Example/DemoPod/DEMOViewController.h
@@ -0,0 +1,13 @@
+//
+//  DEMOViewController.h
+//  DemoPod
+//
+//  Created by zosko on 06/08/2018.
+//  Copyright (c) 2018 zosko. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DEMOViewController : UIViewController
+
+@end
